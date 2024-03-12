ec59c74f6ced1abd66301382ae4ddb9f6e466774
supe4cookie <supe4cookie@gmail.com>
2024-03-12 19:37:04 +0100

    simple setup

---
+++ Makefile
@@ -0,0 +1,31 @@
+CFLAGS = -MMD -MP # -Wall -Wextra -Werror
+NAME = fractol
+
+SRC_PATH = src
+SRC_FILES = main.c
+SRCS = $(addprefix $(SRC_PATH)/, $(SRC_FILES))
+OBJ = $(SRCS:.c=.o)
+OBJDEPS = $(OBJ:.o=.d)
+
+MLX_PATH = minilibx-linux
+MLX_FLAGS = -L$(MLX_PATH) -lmlx -lXext -lX11 -lm
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	cc $(OBJ) $(MLX_FLAGS) -o $(NAME)
+
+%.o: %.c
+	cc $(CFLAGS) -I$(MLX_PATH) -c $< -o $@
+
+-include $(OBJDEPS)
+
+clean:
+	rm -f $(OBJ) $(OBJDEPS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwojtasi <mwojtasi@student.42lyon.fr >     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 18:52:59 by mwojtasi          #+#    #+#             */
+/*   Updated: 2024/03/12 19:18:43 by mwojtasi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minilibx-linux/mlx.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
