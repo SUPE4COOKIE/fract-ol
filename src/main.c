1e045aa34ff1483890b256946df6f579175776fe
supe4cookie <supe4cookie@gmail.com>
2024-03-16 18:44:44 +0100

    uhhh

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mwojtasi <mwojtasi@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 18:52:59 by mwojtasi          #+#    #+#             */
-/*   Updated: 2024/03/13 11:31:03 by mwojtasi         ###   ########.fr       */
+/*   Updated: 2024/03/16 18:44:17 by mwojtasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,13 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned int calculate_color(size_t iterations, size_t max_iterations) {
-    if (iterations == max_iterations) {
-        return 0x00000000; // Black for points inside the Mandelbrot set
-    }
-
-    // Calculate a gradient value between 0 and 255
-    double t = (double)iterations / (double)max_iterations;
-    unsigned char red = (unsigned char)(9 * (1 - t) * t * t * t * 255);
-    unsigned char green = (unsigned char)(15 * (1 - t) * (1 - t) * t * t * 255);
-    unsigned char blue =  (unsigned char)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
-
-    return (red << 16) | (green << 8) | blue; // Combining the colors into a single integer
-}
-
 t_complex pix_to_complex(size_t x, size_t y)
 {
 	t_complex c;
 
-	double minRe = -2.0;
-    double maxRe = 1.0;
-    double minIm = -1.2;
-    double maxIm = minIm + (maxRe - minRe) * HEIGHT / WIDTH;
-
-    c.re = minRe + (x / (double)WIDTH) * (maxRe - minRe);
-    c.im = maxIm - (y / (double)HEIGHT) * (maxIm - minIm);
-
-    return c;
+	c.re = (x - (WIDTH / 2.0)) * 4.0 / WIDTH;
+	c.im = (y - (HEIGHT / 2.0)) * 4.0 / HEIGHT;
+	return (c);
 }
 
 t_complex square_complex(t_complex z)
@@ -84,6 +64,29 @@ size_t mandelbrot(t_complex c)
 	return (i);
 }
 
+void	render(int i, t_mlx_data data)
+{
+	size_t x, y;
+
+	y = 0;
+		while (y < HEIGHT)
+		{
+			x = 0;
+			while (x < WIDTH)
+			{
+				// map pixel to complex plane (???)
+				t_complex c = pix_to_complex(x, y);
+				size_t i = mandelbrot(c);
+				size_t offset = (y * data.line_length) + (x * (data.bits_per_pixel / 8));
+				unsigned int color = gradientColor((float)i / 100);
+				*(unsigned int *)(data.pixels + offset) = color;
+	
+				x++;
+			}
+			y++;
+		}
+}
+
 int main(void)
 {
     t_mlx_data data;
@@ -93,25 +96,7 @@ int main(void)
     data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Mandelbrot");
     data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
     data.pixels = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-
-	y = 0;
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			// map pixel to complex plane (???)
-			t_complex c = pix_to_complex(x, y);
-			size_t i = mandelbrot(c);
-			size_t offset = (y * data.line_length) + (x * (data.bits_per_pixel / 8));
-
-			unsigned int color = calculate_color(i, 100);
-			*(unsigned int *)(data.pixels + offset) = color;
-
-			x++;
-		}
-		y++;
-	}
+	render(-1, data);
 
     mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
     mlx_loop(data.mlx);
