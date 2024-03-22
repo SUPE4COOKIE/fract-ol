8a0ec81db4317269c6bab79d3fc80dccc89fb8cf
supe4cookie <supe4cookie@gmail.com>
2024-03-22 13:27:06 +0100

    changed color handlin and added Julia

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwojtasi <mwojtasi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: mwojtasi <mwojtasi@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 18:52:59 by mwojtasi          #+#    #+#             */
-/*   Updated: 2024/03/17 19:45:33 by mwojtasi         ###   ########.fr       */
+/*   Updated: 2024/03/22 13:26:39 by mwojtasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <X11/keysym.h>
 
 unsigned	int	get_color(float i)
 {
 	unsigned int color;
 
 	color = 0;
-	color += (unsigned int)(9 * (1 - i) * pow(i, 3) * 255) << 16;
-	color += (unsigned int)(15 * pow((1 - i), 2) * pow(i, 2) * 255) << 8;
-	color += (unsigned int)(8.5 * pow((1 - i), 3) * i * 255);
+	if (i == 50)
+		return (0x000000);
+	i = sqrt(i / 50.0);
+	color += (unsigned int)((i) * 255) << 16;
+	color += (unsigned int)((i) * 255) << 8;
+	color += (unsigned int)((i) * 255) * 255;
 	return (color);
 }
 
@@ -31,8 +35,8 @@ t_complex pix_to_complex(size_t x, size_t y, t_mlx_data data)
 {
 	t_complex c;
 
-	c.re = (x - (WIDTH / 2.0)) * (4.0 / WIDTH) / data.zoom + data.center.re;
-	c.im = (y - (HEIGHT / 2.0)) * (4.0 / HEIGHT) / data.zoom + data.center.im;
+	c.re = (x - (WIDTH / 2.0)) * (4.0 / WIDTH) / data.zoom;
+	c.im = (y - (HEIGHT / 2.0)) * (4.0 / HEIGHT) / data.zoom;
 	return (c);
 }
 
@@ -53,6 +57,22 @@ char	has_reached_limit(t_complex z)
 	return (0);
 }
 
+size_t julia(t_complex z)
+{
+    t_complex complex_tmp;
+    size_t i = 0;
+
+    while (i < 50 && !has_reached_limit(z))
+    {
+        // square complex
+        complex_tmp = square_complex(z);
+        z.re = complex_tmp.re - 0.5251993;
+        z.im = complex_tmp.im + 0.5251993;
+        i++;
+    }
+    return i;
+}
+
 size_t mandelbrot(t_complex c)
 {
 	t_complex	z;
@@ -62,7 +82,7 @@ size_t mandelbrot(t_complex c)
 	z.re = 0;
 	z.im = 0;
 	i = 0;
-	while (i < 30 && !has_reached_limit(z))
+	while (i < 50 && !has_reached_limit(z))
 	{
 		//square complex
 		complex_tmp = square_complex(z);
@@ -90,9 +110,9 @@ void	render(t_mlx_data data)
 				t_complex c = pix_to_complex(x, y, data);
 				c.re += data.x_offset;
 				c.im += data.y_offset;
-				size_t i = mandelbrot(c);
+				size_t i = julia(c);
 				size_t offset = (y * data.line_length) + (x * (data.bits_per_pixel / 8));
-				unsigned int color = get_color((float)i / 30);
+				unsigned int color = get_color((float)i);
 				*(unsigned int *)(data.pixels + offset) = color;
 	
 				x++;
@@ -104,13 +124,13 @@ void	render(t_mlx_data data)
 int	key_hook(int keycode, t_mlx_data *data)
 {
 	printf("keycode = %d\n", keycode);
-	if (keycode == 65361)
+	if (keycode == XK_Left)
 		data->x_offset -= data->move_value;
-	if (keycode == 65363)
+	if (keycode == XK_Right)
 		data->x_offset += data->move_value;
-	if (keycode == 65362)
+	if (keycode == XK_Up)
 		data->y_offset -= data->move_value;
-	if (keycode == 65364)
+	if (keycode == XK_Down)
 		data->y_offset += data->move_value;
 	if (keycode == 65451)
 	{
@@ -139,8 +159,6 @@ void	call_hooks(t_mlx_data *data)
 	data->x_offset = 0;
 	data->y_offset = 0;
 	data->zoom = 1;
-	data->center.re = 0;
-	data->center.im = 0;
 	data->move_value = 0.1;
 	mlx_hook(data->win, 17, 0, close_hook, data);
 	mlx_key_hook(data->win, key_hook, data);
